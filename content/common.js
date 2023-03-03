1d8f4e3cbd40ed0eb4eba5100c4e4eb5adf41772
Nicolas Fränkel <nicolas@frankel.ch>
2023-03-03 16:59:45 +0100

    Add a logging statement on the content script to give user feedback

--- content/common.js
+++ content/common.js
@@ -1,6 +1,7 @@
 browser.runtime.onMessage.addListener((message, sender) => {
     const conference = getConference()
     const isAsync = getConference.constructor.name === 'AsyncFunction'
+    window.alert(`Conference ${conference.name} parsed successfully`)
     if (isAsync) {
         return conference
     } else {
